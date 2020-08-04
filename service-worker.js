9e2fa4d884132e0037e576aec5d3b6b23e344ea2
rustielin <rustielin@users.noreply.github.com>
2020-08-04 02:51:35 +0000

    Deploying to gh-pages from  @ ea5329a369f930064bdffbc221fb4882e0133c62 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e285a8ee6cf1be02b119de6edb5424e5.js"
+  "./precache-manifest.1f462b1dbe62612ad36d5ca0cd7090b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
